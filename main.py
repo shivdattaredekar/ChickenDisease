aed255cd94dfb25e6112fec8f125a731fc1a6e50
Shivdatta redekar <shivdattaredekar@gmail.com>
2024-05-15 14:54:55 +0530

    .

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from cnnClassifier.logging import logger
 from cnnClassifier.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
